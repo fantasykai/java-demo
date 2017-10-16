3f6b86e7534fe0c19e57d5de55b9f2f8243602f7
makai <kai.fantasy@gmail.com>
2017-10-16 16:07:49 +0800

    kai.fantasy: java demo项目

---
+++ java-multithread/src/main/java/com/fantasykai/java/App.java
@@ -0,0 +1,13 @@
+package com.fantasykai.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

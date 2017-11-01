a370086b35299e9c3bb799563efb8990427f2144
makai <kai.fantasy@gmail.com>
2017-11-01 16:15:02 +0800

    kai.fantasy: 循环while

---
+++ java-base/src/main/java/com/fantasykai/java/base/Retirement2.java
@@ -0,0 +1,40 @@
+package com.fantasykai.java.base;
+
+import java.util.Scanner;
+
+public class Retirement2 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("How much money will you contribute every year?");
+        double payment = in.nextDouble();
+
+        System.out.println("Interest rate in %: ");
+        double interestRate = in.nextDouble();
+
+        double balance = 0;
+        int years = 0;
+
+        String input;
+
+        do {
+
+            balance += payment;
+            double interest = balance * interestRate / 100;
+            balance += interest;
+
+            years++;
+
+            System.out.printf("After year %d, your balance is %,.2f%n ", years, balance);
+
+            System.out.println("Ready do retire? (Y/N) ");
+
+            input = in.next();
+        }
+        while (input.equals("N"));
+
+    }
+
+}

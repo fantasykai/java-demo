3328ef1374e4e4239aa00879b24d87ea1d1b016a
makai <kai.fantasy@gmail.com>
2017-11-05 21:46:03 +0800

    kai.fantasy: 时间工具

---
+++ java-tools/src/main/java/com/fantasykai/java/date/DateUtils.java
@@ -0,0 +1,14 @@
+package com.fantasykai.java.date;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈功能详细描述〉
+ * <p>
+ *
+ * @author Created by makai on 2017/11/05 .
+ * @see [相关类/方法]（可选）
+ * @since [产品 /模块版本] （可选）
+ */
+public class DateUtils {
+
+}

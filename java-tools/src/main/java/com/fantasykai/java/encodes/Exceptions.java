0fa27b85cf637c49b43b3e8ea35ff73905153e71
makai <kai.fantasy@gmail.com>
2017-11-08 19:29:17 +0800

    kai.fantasy: 编解码工具

---
+++ java-tools/src/main/java/com/fantasykai/java/encodes/Exceptions.java
@@ -0,0 +1,53 @@
+package com.fantasykai.java.encodes;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈功能详细描述〉
+ * <p>
+ *
+ * @author Created by makai on 2017/11/08 .
+ * @see [相关类/方法]（可选）
+ * @since [产品 /模块版本] （可选）
+ */
+public class Exceptions {
+
+    /**
+     * 将CheckedException转换为UncheckedException.
+     */
+    public static RuntimeException unchecked(Exception e) {
+        if (e instanceof RuntimeException) {
+            return (RuntimeException) e;
+        } else {
+            return new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 将ErrorStack转化为String.
+     */
+    public static String getStackTraceAsString(Exception e) {
+        StringWriter stringWriter = new StringWriter();
+        e.printStackTrace(new PrintWriter(stringWriter));
+        return stringWriter.toString();
+    }
+
+    /**
+     * 判断异常是否由某些底层的异常引起.
+     */
+    public static boolean isCausedBy(Exception ex, Class<? extends Exception>... causeExceptionClasses) {
+        Throwable cause = ex.getCause();
+        while (cause != null) {
+            for (Class<? extends Exception> causeClass : causeExceptionClasses) {
+                if (causeClass.isInstance(cause)) {
+                    return true;
+                }
+            }
+            cause = cause.getCause();
+        }
+        return false;
+    }
+
+}
